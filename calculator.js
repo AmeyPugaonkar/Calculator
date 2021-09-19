93ae9983167b5ecd95fe1b995f4238ff6d373aee
AmeyPugaonkar <90090291+AmeyPugaonkar@users.noreply.github.com>
2021-09-19 09:24:20 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,48 @@
+let displayBtn = document.getElementsByClassName("display");
+
+// on reload window
+window.onload= ()=> {
+    for (let i = 0; i < displayBtn.length; i++) {
+        displayBtn[i].setAttribute("onclick","sreenDisplay(this)"); 
+    }
+}
+
+let screenvalue="";
+let calucateValue ="";
+
+// diaplay the clicked values on screen
+function sreenDisplay(element){
+    let val = element.id;
+    let value=element.innerHTML;
+    if(val == "multi"){
+        calucateValue=calucateValue+"*";
+    }else if(val == "divide"){
+        calucateValue=calucateValue+"/";
+    }else{
+        calucateValue=calucateValue+value;
+    }
+    screenvalue=screenvalue+value;
+    document.getElementById("result").value = screenvalue;
+}
+
+// Main calculation
+function calculate(){
+    let answer = eval(calucateValue);
+    document.getElementById("result").value = answer;
+    screenvalue=answer;
+    calucateValue = answer;
+}
+
+// clear the screen
+function clearAll(){
+    screenvalue="";
+    calucateValue="";
+    document.getElementById("result").value = screenvalue;
+}
+
+// remove last element
+function removeLast(){
+    screenvalue = screenvalue.substring(0,screenvalue.length-1);
+    calucateValue = calucateValue.substring(0,calucateValue.length-1);
+    document.getElementById("result").value = screenvalue;
+}
